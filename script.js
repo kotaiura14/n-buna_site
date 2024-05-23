34a6b4172a5d14cb61cb28ae192a2376ec418c29
kotaiura14 <sitecreatekou@gmail.com>
2024-05-24 00:40:26 +0900

    ボカロのスライドショー機能実装、サイズ変更必要

--- script.js
+++ script.js
@@ -28,4 +28,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 初期状態
     nBunaTab.click();
+    
 });
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.main-title, .section-title', {
+    delay: 200,
+    origin: 'left'
+});
+ScrollReveal().reveal('.sec-01 .image, .sec-02 .image, .sec-03 .image, .sec-04 .image, .sec-05 .image, .sec-06 .image', {
+    delay: 500,
+    origin: 'top'
+});
+ScrollReveal().reveal('.text-box, .info', {
+    delay: 600,
+    origin: 'bottom'
+});
+
+// Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
